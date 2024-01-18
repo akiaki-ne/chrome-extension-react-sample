e0173d45797667eff190000b8260d24fac2d8ec3
nichijousummit <nichijousummit@gmail.com>
2024-01-18 21:48:58 +0900

    update: 選択した言語を保持してデフォルトで選択する処理を追加

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,7 +1,30 @@
-import { Counter } from '../app/features/counter';
+import React from 'react';
+import { getBucket } from '@extend-chrome/storage';
+
+type MyBucket = {
+  targetLang: string;
+};
+
+const bucket = getBucket<MyBucket>('my_bucket', 'sync');
 
 const Popup = () => {
   document.body.className = 'w-[30rem] h-[15rem] flex items-center justify-center';
+
+  const [lang, setLang] = React.useState('EN');
+
+  React.useEffect(() => {
+    (async () => {
+      const { targetLang } = await bucket.get();
+      if (targetLang) {
+        setLang(targetLang);
+      }
+    })();
+  }, []);
+
+  const saveLang = (lang: string) => {
+    bucket.set({ targetLang: lang });
+    setLang(lang);
+  };
 
   return (
     <div className="bg-white shadow rounded-lg p-6">
@@ -10,6 +33,8 @@ const Popup = () => {
         <select
           name="target_lang"
           className="form-select mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          onChange={(event: React.ChangeEvent<HTMLSelectElement>) => saveLang(event.target.value)}
+          value={lang}
         >
           <option value="EN">英語</option>
           <option value="KO">韓国語</option>
@@ -17,7 +42,6 @@ const Popup = () => {
           <option value="JA">日本語</option>
         </select>
       </div>
-      {/* <Counter /> */}
     </div>
   );
 };
